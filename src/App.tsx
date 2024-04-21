c44039a90d07ec25a32843401d1f85d1ec1cb98f
SeungMin Shin <sky2197kr@gmail.com>
2024-04-21 16:51:29 +0900

    [TODO-1] 코드 스플릿팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import IndexPage from './pages/index';
-import ListPage from './pages/list';
-import DetailPage from './pages/detail';
 import reportWebVitals from './reportWebVitals';
+import { Suspense, lazy } from 'react';
+
+const IndexPage = lazy(() => import('./pages/index'));
+const ListPage = lazy(() => import('./pages/list'));
+const DetailPage = lazy(() => import('./pages/detail'));
 
 const App = () => {
+
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -13,11 +17,13 @@ const App = () => {
           * [로딩 최적화 - 코드 스플릿팅] 
           * 각 페이지 별로 필요한 모듈이 따로 있지만 하나의 번들파일로 되어 있습니다.
         */}
-        <Routes>
-          <Route path="/" element={<IndexPage />} />
-          <Route path="/list" element={<ListPage />} />
-          <Route path="/detail/:id" element={<DetailPage />} />
-        </Routes>
+        <Suspense>
+          <Routes>
+            <Route path="/" element={<IndexPage />} />
+            <Route path="/list" element={<ListPage />} />
+            <Route path="/detail/:id" element={<DetailPage />} />
+          </Routes>
+        </Suspense>
       </BrowserRouter>
     </div>
   );
